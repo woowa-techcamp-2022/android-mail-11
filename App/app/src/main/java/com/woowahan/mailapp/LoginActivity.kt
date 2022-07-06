2ed774448454b9831f441124aeae6a1c458f569a
Youngmu <tyg04308@naver.com>
2022-07-06 10:37:19 +0900

    [ADD] #1 - LoginActivity

--- App/app/src/main/java/com/woowahan/mailapp/LoginActivity.kt
+++ App/app/src/main/java/com/woowahan/mailapp/LoginActivity.kt
@@ -1,9 +1,10 @@
 package com.woowahan.mailapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
