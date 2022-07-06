7b7e0a01be513fce41bc98f687c09573cabc39db
Youngmu <tyg04308@naver.com>
2022-07-06 11:23:39 +0900

    [FEAT] #1 - email check

--- App/app/src/main/java/com/woowahan/mailapp/LoginActivity.kt
+++ App/app/src/main/java/com/woowahan/mailapp/LoginActivity.kt
@@ -16,6 +16,7 @@ class LoginActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
 
         binding.nicknameEditText.addTextChangedListener(nicknameWatcher)
+        binding.emailEditText.addTextChangedListener(emailWatcher)
     }
 
     private val nicknameWatcher = object : TextWatcher {
@@ -27,20 +28,54 @@ class LoginActivity : AppCompatActivity() {
 
         override fun afterTextChanged(s: Editable?) {
             val nickname = binding.nicknameEditText.text.toString()
+            val email = binding.emailEditText.text.toString()
 
             if (isValidNickname(nickname)) {
-                binding.nextBtn.isEnabled = true
                 binding.nicknameInputLayout.isEndIconVisible = false
                 binding.nicknameInputLayout.error = null
 
             } else {
-                binding.nextBtn.isEnabled = false
                 binding.nicknameInputLayout.isEndIconVisible = true
                 binding.nicknameInputLayout.error = "닉네임은 영문과 숫자를 결합한 4~12자로 입력해주세요."
             }
+
+            binding.nextBtn.isEnabled =
+                isValidNickname(nickname) && isValidEmail(email)
         }
 
     }
+
+    private val emailWatcher = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+        }
+
+        override fun afterTextChanged(s: Editable?) {
+            val nickname = binding.nicknameEditText.text.toString()
+            val email = binding.emailEditText.text.toString()
+
+            if (isValidEmail(email)) {
+                binding.emailInputLayout.isEndIconVisible = false
+                binding.emailInputLayout.error = null
+
+            } else {
+                binding.emailInputLayout.isEndIconVisible = true
+                binding.emailInputLayout.error = "올바른 이메일 형식이 아닙니다."
+            }
+
+            binding.nextBtn.isEnabled =
+                isValidNickname(nickname) && isValidEmail(email)
+        }
+    }
+
+    private fun isValidEmail(email: String): Boolean {
+        val pattern = android.util.Patterns.EMAIL_ADDRESS;
+
+        return pattern.matcher(email).find()
+    }
+
 
     private fun isValidNickname(nickname: String): Boolean {
         val nicknameRegex = "^(?=.*[A-Za-z])(?=.*[0-9])[A-Za-z[0-9]]{4,12}$"
